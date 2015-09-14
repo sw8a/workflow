f1e0acad954072fd783640472bb3940618513e4d
Gonzalo Ziadi <gziadi@ufl.edu>
2015-09-14 13:39:10 -0400

    Added enter key functionality to add to do items

--- todo-src/script.js
+++ todo-src/script.js
@@ -14,56 +14,74 @@ myApp.controller('MainCtrl', function($scope) {
         done: false
     }];
     $scope.newItem = "";
-    $scope.newPrio = "";    // Stored selected priority for todo item (KC)
+    $scope.newPrio = ""; // Stored selected priority for todo item (KC)
 
     $scope.addItem = function() {
-        console.log("in add");
         if ($scope.newItem !== "") {
-            $scope.todos.push({text:$scope.newItem, edit:false, prio:"now"});
+            $scope.todos.push({
+                text: $scope.newItem,
+                edit: false,
+                prio: "now"
+            });
             $scope.newItem = "";
         }
     }
 
-    
+    /*
+        Added by: Gonzalo
+        Task #7: Add todo items with enter rather than add button.
+    */
+    $scope.enterAddItem = function($event){
+      var keyCode = $event.which || $event.keyCode;
+      if (keyCode === 13) {
+          $scope.addItem();
+      }
+    };
+
     $scope.deleteItem = function(item) {
-        console.log("in delete");
         var index = $scope.todos.indexOf(item);
         $scope.todos.splice(index, 1);
     }
-// function to delete all items. Stores current todos in a temp list, clears $scope.todos, and then repopulates it with uncompleted tasks
-    $scope.deleteCompletedItems = function ()
-    {
-     var oldTodos = $scope.todos; 
-     $scope.todos = [];
-     angular.forEach(oldTodos,function(item)
-     {
-      if(!item.done)
-      {
-        $scope.todos.push(item);
-      }
-     });
+
+    // function to delete all items. Stores current todos in a temp list, clears $scope.todos, and then repopulates it with uncompleted tasks
+    $scope.deleteCompletedItems = function() {
+        var oldTodos = $scope.todos;
+        $scope.todos = [];
+        angular.forEach(oldTodos, function(item) {
+            if (!item.done) {
+                $scope.todos.push(item);
+            }
+        });
     }
+
+    /*
+        Added by: Gonzalo
+        Task #3: Add edit button functionality.
+    */
     $scope.editItem = function(item) {
-      var index = $scope.todos.indexOf(item);
+        var index = $scope.todos.indexOf(item);
 
-      if ($scope.todos[index].edit === true) {
-           $scope.todos[index].edit = false;
-       } else {
-           $scope.todos[index].edit = true;
-       }
+        if ($scope.todos[index].edit === true) {
+            $scope.todos[index].edit = false;
+        } else {
+            $scope.todos[index].edit = true;
+        }
     }
 
+    /*
+        Added by: Gonzalo
+        Task #3: Add edit button functionality - submit edit with enter key.
+    */
     $scope.keyEnter = function($event, item) {
-       var index = $scope.todos.indexOf(item);
+        var index = $scope.todos.indexOf(item);
 
-       if ($event.keyCode == 13) {
-           $scope.todos[index].edit = false;
-       }
-   }
+        if ($event.keyCode == 13) {
+            $scope.todos[index].edit = false;
+        }
+    }
 
-   $scope.completeItem = function(item) {
-    item.done = !item.done;
-    console.log("done")
+    $scope.completeItem = function(item) {
+        item.done = !item.done;
     }
 
     // Added by: KC
