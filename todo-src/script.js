6370b66c88d031f42c6145d43caf1732073cc35f
Ricky Rodriguez <ricardodrodrigue@ufl.edu>
2015-09-13 21:36:11 -0400

    Add Delete Completed Items Functionality
    Task 6 is complete

--- todo-src/script.js
+++ todo-src/script.js
@@ -21,12 +21,25 @@ myApp.controller('MainCtrl', function($scope) {
         }
     }
 
+    
     $scope.deleteItem = function(item) {
         console.log("in delete");
         var index = $scope.todos.indexOf(item);
         $scope.todos.splice(index, 1);
     }
-
+// function to delete all items. Stores current todos in a temp list, clears $scope.todos, and then repopulates it with uncompleted tasks
+    $scope.deleteCompletedItems = function ()
+    {
+     var oldTodos = $scope.todos; 
+     $scope.todos = [];
+     angular.forEach(oldTodos,function(item)
+     {
+      if(!item.done)
+      {
+        $scope.todos.push(item);
+      }
+     });
+    }
     $scope.editItem = function(item) {
       var index = $scope.todos.indexOf(item);
 
