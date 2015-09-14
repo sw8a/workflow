52f3cd36084596a325414073b2a757f19c48eb34
gavingreco <gavingreco@live.com>
2015-09-14 12:53:36 -0400

    update count on script.js

--- todo-src/script.js
+++ todo-src/script.js
@@ -62,6 +62,14 @@ myApp.controller('MainCtrl', function($scope) {
     item.done = !item.done;
     console.log("done")
     }
+    
+    $scope.remaining = function() {
+        var count = 0;
+        angular.forEach($scope.todoList.todos, function(todo) {
+            count += todo.done ? 0 : 1;
+        });
+        return count;
+    };
 
 
 });
